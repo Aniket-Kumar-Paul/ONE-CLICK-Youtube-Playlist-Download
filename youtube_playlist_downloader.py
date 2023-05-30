c43c32581c28b39bceac005843eb2ba14570c9a3
Aniket <aniketkumarpaul1@gmail.com>
2023-05-30 22:13:04 +0530

    Added youtube_playlist_downloader.py

---
+++ youtube_playlist_downloader.py
@@ -0,0 +1,38 @@
+from pytube import Playlist
+from pytube.exceptions import AgeRestrictedError, VideoPrivate
+from tqdm import tqdm
+import re
+
+def replace_special_characters(input_string):
+    pattern = r'[^a-zA-Z0-9_]'
+    replaced_string = re.sub(pattern, ' ', input_string)
+    return replaced_string
+
+link = input("Enter Youtube Video Playlist Link: ")
+yt_playlist = Playlist(link)
+total_videos = len(yt_playlist.videos)
+print("Total number of videos:", total_videos)
+
+with tqdm(total=len(yt_playlist.videos), desc="Downloading", unit="video") as pbar:
+    for i, video in enumerate(yt_playlist.videos):
+        title = replace_special_characters(video.title)
+        print("\nDownloading:", (i + 1), title)
+        
+        try:
+            # Get the stream
+            stream = video.streams.get_by_resolution("720p")
+
+            # Get the total file size
+            file_size = stream.filesize
+
+            # Download the video
+            stream.download(filename=str(i + 1) + "_" + title + ".mp4")
+            
+            print(f'{i + 1}/{total_videos} videos downloaded')
+        except AgeRestrictedError:
+            print("Skipping age-restricted video:", title)
+        except VideoPrivate:
+            print("Skipping private video", title)
+        pbar.update(1)
+        
+print("\nAll videos are downloaded.")
